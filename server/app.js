9e3e87ab5fc06645fefe65cda3c45c176052ff7d
Raahul-bale <Raahul-bale@users.noreply.github.com>
2026-09-24 13:59:45 +0530

    Add mongoose dependency, start script, and dynamic PORT for Render deployment

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ app.use("/api/courses", courseRoute);
 
 connectDB();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`server is running on http://localhost:${PORT}`);
 });
